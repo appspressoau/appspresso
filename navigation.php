cdb5e6145c2e0b60a9246d7f00ae4ebbd1904741
Ashley Holman <ashley@netspot.com.au>
2012-11-17 22:19:53 +1030

    Fix linking between pages so that jquery mobile links still work.

--- navigation.php
+++ navigation.php
@@ -1,9 +1,9 @@
 <div id="footer">
   <div id="global-nav" data-role="controlgroup" data-type="horizontal">
-        <a href="1.php" data-role="button"><i class="icon-group"></i></a>
-        <a href="2.php" data-role="button"><i class="icon-reorder"></i></a>
-        <a href="3.php" data-role="button"><i class="icon-map-marker"></i></a>
-        <a href="4.php" data-role="button"><i class="icon-user"></i></a>
-        <a href="5.php" data-role="button"><i class="icon-cogs"></i></a>
+        <a href="1.php" data-role="button" data-ajax="false"><i class="icon-group"></i></a>
+        <a href="2.php" data-role="button" data-ajax="false"><i class="icon-reorder"></i></a>
+        <a href="3.php" data-role="button" data-ajax="false"><i class="icon-map-marker"></i></a>
+        <a href="4.php" data-role="button" data-ajax="false"><i class="icon-user"></i></a>
+        <a href="5.php" data-role="button" data-ajax="false"><i class="icon-cogs"></i></a>
     </div>
 </div>
