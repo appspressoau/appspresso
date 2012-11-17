5633fdafa420f0a6a05b029f8dd51a4367bfbf3d
Zoe Bogner <zbogner@gmail.com>
2012-11-17 12:29:30 +1030

    Splitting nag into own file

---
+++ navigation.php
@@ -0,0 +1,9 @@
+<div id="footer">
+  <div data-role="controlgroup" data-type="horizontal">
+<a href="index.html" data-role="button">1</a>
+<a href="index.html" data-role="button">2</a>
+<a href="index.html" data-role="button">3</a>
+<a href="index.html" data-role="button">4</a>
+<a href="index.html" data-role="button">5</a>
+</div>
+</div>
