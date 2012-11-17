ec7b991da779d53282906767eff12636f55a5876
Zoe Bogner <zbogner@gmail.com>
2012-11-17 12:25:41 +1030

    Changed from html to php

--- template.php
+++ template.php
@@ -11,6 +11,7 @@
     <link rel="apple-touch-startup-image" href="img/splash.png"><!-- Fullscreen app -->
     <meta name="apple-mobile-web-app-capable" content="yes"><!-- Alter the staus bar (default|black|black-translucent) -->
     <meta name="apple-mobile-web-app-status-bar-style" content="black"><!-- Le styles -->
+
     <link href="./styles/css/bootstrap.css" rel="stylesheet" type="text/css">
     <style type="text/css">
   body {
@@ -27,31 +28,11 @@
         margin: 30px 0;
       }
 
-      /* Main marketing message and sign up button */
-      .jumbotron {
-        margin: 60px 0;
-        text-align: center;
-      }
-      .jumbotron h1 {
-        font-size: 72px;
-        line-height: 1;
-      }
-      .jumbotron .btn {
-        font-size: 21px;
-        padding: 14px 24px;
-      }
-
-      /* Supporting marketing content */
-      .marketing {
-        margin: 60px 0;
-      }
-      .marketing p + h4 {
-        margin-top: 28px;
-      }
     </style>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./styles/css/responsive.css" rel="stylesheet" type="text/css">
-    <link href="./styles/css/jquery.mobile.structure-1.2.0.css" rel="stylesheet" type="text/css"><!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <link href="./styles/css/jquery.mobile-1.2.0.css" rel="stylesheet" type="text/css">
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="scripts/js/html5shiv.js"></script>
     <![endif]-->
@@ -73,29 +54,21 @@
 
                 <div data-role="page" id="one">
                     <div data-role="header">
-                        <h1>Multi-page</h1>
+                        <h1>Appacino</h1>
                     </div><!-- /header -->
 
                     <div data-role="content">
-                        <h2>One</h2>
 
-                        <p>I have an <code>id</code> of "one" on my page container. I'm first in the source order so I'm shown when the page loads.</p>
+                        <!--
+<p><a href="#two" data-role="button">Show page "two"</a></p>
 
-                        <p>This is a multi-page boilerplate template that you can copy to build your first jQuery Mobile page. This template contains multiple "page" containers inside, unlike a <a href="page-template.html">single page template</a> that has just one page within it.</p>
-
-                        <p>Just view the source and copy the code to get started. All the CSS and JS is linked to the jQuery CDN versions so this is super easy to set up. Remember to include a meta viewport tag in the head to set the zoom level.</p>
-
-                        <p>You link to internal pages by referring to the <code>id</code> of the page you want to show. For example, to <a href="#two">link</a> to the page with an <code>id</code> of "two", my link would have a <code>href="#two"</code> in the code.</p>
-
-                        <h3>Show internal pages:</h3>
-
-                        <p><a href="#two" data-role="button">Show page "two"</a></p>
-
-                        <p><a href="#popup" data-role="button" data-rel="dialog" data-transition="pop">Show page "popup" (as a dialog)</a></p>
+-->
+                        <p><a href="#popup-login" data-role="button" data-rel="dialog" data-transition="pop">Login</a></p>
+                        <p><a href="#popup-register" data-role="button" data-rel="dialog" data-transition="pop">Register</a></p>
                     </div><!-- /content -->
 
                     <div data-role="footer" data-theme="d">
-                        <h4>Page Footer</h4>
+                        <?php require_once("navigation.php");?>
                     </div><!-- /footer -->
                 </div><!-- /page one -->
                 <!-- Start of second page: #two -->
@@ -121,17 +94,34 @@
                 </div><!-- /page two -->
                 <!-- Start of third page: #popup -->
 
-                <div data-role="page" id="popup">
+                <div data-role="page" id="popup-login">
                     <div data-role="header" data-theme="e">
-                        <h1>Dialog</h1>
+                        <h1>Login</h1>
                     </div><!-- /header -->
-
                     <div data-role="content" data-theme="d">
-                        <h2>Popup</h2>
+                        <form>
+                            <input type="text" placeholder="Email">
+                            <input type="password" placeholder="Password">
+                        </form>
+                        <p><a href="#feed" data-role="button" data-inline="true" data-icon="back">Login</a></p>
+                        <p><a href="#one" data-rel="back" data-role="button" data-inline="true" data-icon="back">Back</a></p>
+                    </div><!-- /content -->
 
-                        <p>I have an id of "popup" on my page container and only look like a dialog because the link to me had a <code>data-rel="dialog"</code> attribute which gives me this inset look and a <code>data-transition="pop"</code> attribute to change the transition to pop. Without this, I'd be styled as a normal page.</p>
+                    <div data-role="footer">
+                        <h4>Page Footer</h4>
+                    </div><!-- /footer -->
+                </div><!-- /page popup-login -->
 
-                        <p><a href="#one" data-rel="back" data-role="button" data-inline="true" data-icon="back">Back to page "one"</a></p>
+                <div data-role="page" id="popup-register">
+                    <div data-role="header" data-theme="e">
+                        <h1>Register</h1>
+                    </div><!-- /header -->
+                    <div data-role="content" data-theme="d">
+                        <form>
+                            <input type="text" placeholder="Email">
+                            <input type="password" placeholder="Password">
+                        </form>
+                        <p><a href="#feed" data-role="button" data-inline="true" data-icon="back">Register</a></p>
                     </div><!-- /content -->
 
                     <div data-role="footer">
