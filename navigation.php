e7c38a4e3eb1eb7b96200b4bbed8d208c5f7a65b
Zoe Bogner <zbogner@gmail.com>
2012-11-17 13:45:36 +1030

    Page templates

--- navigation.php
+++ navigation.php
@@ -1,9 +1,9 @@
 <div id="footer">
-  <div data-role="controlgroup" data-type="horizontal">
-<a href="index.html" data-role="button">1</a>
-<a href="index.html" data-role="button">2</a>
-<a href="index.html" data-role="button">3</a>
-<a href="index.html" data-role="button">4</a>
-<a href="index.html" data-role="button">5</a>
-</div>
+  <div id="global-nav" data-role="controlgroup" data-type="horizontal">
+        <a href="1.php" data-role="button"><i class="icon-group"></i></a>
+        <a href="2.php" data-role="button"><i class="icon-reorder"></i></a>
+        <a href="3.php" data-role="button"><i class="icon-map-marker"></i></a>
+        <a href="4.php" data-role="button"><i class="icon-user"></i></a>
+        <a href="5.php" data-role="button"><i class="icon-cogs"></i></a>
+    </div>
 </div>
